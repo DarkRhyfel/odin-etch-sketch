93dc750a0f2c9c2986347bda7c95b0f5a7c1908e
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-05 15:34:46 -0500

    Added function to clear button.
    Added a new function to clear the grid and ask for a new size.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@
 const container = document.querySelector('div.container');
 const grid = document.querySelector('div.grid');
 
+generateGrid();
+
+const clearOption = document.querySelector('#clear');
+
 // Function to color a div when mouse enters it
 function colorTile(e) {
     let tile = e.target;
@@ -9,19 +13,43 @@ function colorTile(e) {
     tile.classList.add('colored');
 }
 
-// Adding divs to container
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+// Function to add divs to container
+function generateGrid(size = 16) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
 
-    for (let j = 0; j < 16; j++) {
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
+        for (let j = 0; j < size; j++) {
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
 
-        tile.addEventListener('mouseenter', colorTile);
+            tile.addEventListener('mouseenter', colorTile);
 
-        row.appendChild(tile);
+            row.appendChild(tile);
+        }
+
+        grid.appendChild(row);
+    }
+}
+
+// Function to generate a new grid
+function redrawGrid() {
+    let size = parseInt(prompt('Please entre a new size for the grid.', '16'));
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
     }
 
-    grid.appendChild(row);
+    generateGrid(size > 100 ? 100 : size);
 }
+
+// Function to clear grid's content
+function clearGrid(e) {
+    const tiles = document.querySelectorAll('div.tile');
+
+    tiles.forEach(t => t.classList.remove('colored'));
+
+    redrawGrid();
+}
+
+clearOption.addEventListener('click', clearGrid);
