0abc6691857d03278fd963195f04a360e759baf0
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-05 13:50:47 -0500

    Added mouseenter event for tiles
    Added a function to color a tile when its mouseenter event is triggered.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 // Variables initialization
 const container = document.querySelector('div.container');
+const grid = document.querySelector('div.grid');
+
+// Function to color a div when mouse enters it
+function colorTile(e) {
+    let tile = e.target;
+
+    tile.classList.add('colored');
+}
 
 // Adding divs to container
 for (let i = 0; i < 16; i++) {
@@ -10,8 +18,10 @@ for (let i = 0; i < 16; i++) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
 
+        tile.addEventListener('mouseenter', colorTile);
+
         row.appendChild(tile);
     }
 
-    container.appendChild(row);
+    grid.appendChild(row);
 }
